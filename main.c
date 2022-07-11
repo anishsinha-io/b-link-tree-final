bb0c3f6ef8115d69b6e815f4c9b770c109ac0a1b
Anish Sinha <anishsinha0128@gmail.com>
2022-07-11 15:19:19 -0400

    added stack module

--- main.c
+++ main.c
@@ -4,9 +4,12 @@
 #include "slice_test.h"
 #include "string.h"
 #include "strings_test.h"
+#include "stack.h"
+#include "stack_test.h"
 
 int main() {
     test_slice();
     test_strings();
+    test_stack();
     return 0;
 }
